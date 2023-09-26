3c6a2e691bb0dddd36a4caa43ffdc3bc5d35251c
guithin <on14@naver.com>
2023-09-26 11:17:35 +0900

    ws example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 import Router from './Router';
+import io from 'socket.io-client';
+
+const socket = io(import.meta.env.VITE_APP_BASE_URL);
+
+socket.on('connect', () => {
+  console.log('connected');
+});
+
+socket.on('disconnect', () => {
+  console.log('disconnected');
+});
+
+socket.on('ExampleWSE', (data) => {
+  console.log(data);
+});
+
+(window as any).temp = () => socket.emit('ExampleWSL', { id: 11 });
 
 const App: React.FC = () => {
   return (
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,7 @@
 declare module 'process' {
   global {
     interface ImportMetaEnv {
+      VITE_APP_BASE_URL: string;
       [key: string]: string;
     }
   }
